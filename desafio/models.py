835867416cd36d768b97d3d6d01ac8935d20243b
Lucas <montobybianchi@gmail.com>
2022-07-01 08:44:02 -0300

    Commit 1-07-22 a 8hs

--- desafio/models.py
+++ desafio/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 class Familiar(models.Model):
     nombre = models.CharField(max_length=30)
-    phone = models.IntegerField(verbose_name='Telefono')
-    fecha_nacimiento = models.DateField(null=True)
+    edad = models.IntegerField()
+    fecha_creacion = models.DateField(null=True)
+    
--- desafio/urls.py
+++ desafio/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
-from .views import crear, una_vista, un_template
+#from django.urls import path
+# from .views import crear, una_vista, un_template
 
-urlpatterns = [
-    path('', una_vista),
-    path('un-template', un_template),
-    path('crear', crear),
+# urlpatterns = [
+#     path('', una_vista),
+#     path('un-template', un_template),
+#     path('crear', crear),
     
-]
+# ]
 
