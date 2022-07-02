b6165325ebf47b590eb97643888069780bb9f7fb
Lucas <montobybianchi@gmail.com>
2022-07-02 09:53:47 -0300

    2-7-22

--- desafio/urls.py
+++ desafio/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import una_vista, crear, listado
 
 urlpatterns = [
-    path('', una_vista, name= 'index'),
-    path('blog/', crear, name='crear'),
+    path('', una_vista, name='index'), 
     path('blog/', listado, name='listado'),
-   
+    path('crear/', crear, name='crear'),
 ]
