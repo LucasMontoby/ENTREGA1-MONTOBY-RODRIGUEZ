6bc9728a61c1ddd1e5d72240b269e9809fcdf9ef
Lucas <montobybianchi@gmail.com>
2022-07-20 20:34:37 -0300

    20-07-22

--- familiares/urls.py
+++ familiares/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('personal/', views.ListadoGatos.as_view(), name='listado1'),
-    path('crear1/', views.CrearGato.as_view(), name='crear1'),
-    path('editar1/<int:pk>/', views.EditarGato.as_view(), name='editar1'),
-    path('eliminar1/<int:pk>/', views.EliminarGato.as_view(), name='eliminar1'),
+    path('personal/', views.Listado1.as_view(), name='listado1'),
+    path('crear1/', views.Crear1.as_view(), name='crear1'),
+    path('editar1/<int:pk>/', views.Editar1.as_view(), name='editar1'),
+    path('eliminar1/<int:pk>/', views.Eliminar1.as_view(), name='eliminar1'),
     # path('mostrar1/<int:pk>/', views.MostrarGato.as_view(), name='mostrar1'),
 ]
+
